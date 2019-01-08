7dca03a795cb7bccdc6dea9cbc4872f15df75f73
samikroon <samikroon@gmail.com>
2019-01-08 12:55:12 +0100

    Update JSONBericht.java

--- src/hanze/nl/infobord/JSONBericht.java
+++ src/hanze/nl/infobord/JSONBericht.java
@@ -1,7 +1,5 @@
 package hanze.nl.infobord;
 
-//import hanze.nl.tijdtools.InfobordTijdFuncties;
-
 public class JSONBericht {
 	private int tijd;
 	private int aankomsttijd;
@@ -73,8 +71,6 @@ public class JSONBericht {
 	}
 
 	public String getInfoRegel() {
-//		InfobordTijdFuncties tijdFuncties = new InfobordTijdFuncties();
-//		String tijd = tijdFuncties.getFormattedTimeFromCounter(aankomsttijd);
 		String tijd = "" + aankomsttijd;
 		String regel = String.format("%8s - %5s - %12s", this.lijnNaam, this.eindpunt, tijd);
 		return regel;
